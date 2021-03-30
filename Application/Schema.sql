1c072ef12d3d4e3acdf1fb93a86ff941e55b2146
peter <peter@Susans-MacBook-Air-2.local>
2021-03-30 11:56:32 -0700

    my 2nd IHP project, redoing the BLog tutorial for more speed, familiarity and playing

---
+++ Application/Schema.sql
@@ -0,0 +1,6 @@
+-- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE posts (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    Title TEXT NOT NULL,
+    Body TEXT NOT NULL
+);
